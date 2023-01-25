e992b52d1fde163ea503248a05086d11f732c302
mrworld196wide <shubhum19@gmail.com>
2023-01-25 16:26:54 +0530

    html added  and JS updated

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-let request = require('request');
-let apiKey = '93f7e909769041592bca79e5b6a03a39';
-let city = 'portland';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+const API_KEY = `93f7e909769041592bca79e5b6a03a39`
+const form = document.querySelector("form")
+const search = document.querySelector("#search")
+const weather = document.querySelector("#weather")
+const getWeather = async(city) => {
+    weather.innerHTML = `<h2> Loading... <h2>`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const response = await fetch(url);
+    const data = await response.json()
+    return showWeather(data)
+}
 
-request(url, function (err, response, body) {
-    if(err){
-      console.log('error:', error);
-    } else {
-      console.log('body:', body);
+const showWeather = (data) => {
+    if (data.cod == "404") {
+        weather.innerHTML = `<h2> City Not Found <h2>`
+        return;
     }
-  });
+    weather.innerHTML = `
+        <div>
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+        </div>
+        <div>
+            <h2>${data.main.temp} ℃</h2>
+            <h4> ${data.weather[0].main} </h4>
+        </div>
+    `
+}
+
+form.addEventListener(
+    "submit",
+    function(event) {
+        getWeather(search.value)
+        event.preventDefault();
+    }
+)
